fd9173d6fe82ae4f3460b14962e668069311d369
Ana <ana.bastos881@gmail.com>
2016-09-18 14:41:12 -0300

    Implements mijo e bola de pelo do gatinho :)
    Adicionando objetos de sujeira e colisao com vassoura para limpar

--- README.md
+++ README.md
@@ -5,3 +5,18 @@ Jogo feito em dois dias na SPJAM 2016 e minha primeira vez usando phaser.io :smi
 
 Atualmente estou rodando por `python -m SimpleHTTPServer 8080`
 
+ULTIMAS HORAS MEUS DEU DO CEU NAO VAI DAR TEMPO BRASIL
+[] Arrumar drag em drop com tutorial do ossinho(nao bugar enquanto anda)
+[] implementar destroy de objetos em grupo
+[] implementar perder vida e gato destroy apos 3 segundo de um status negativo
+[] implementar game overs
+[x] implementar walk com twins
+[x] balaozinho funfar suave na colisao
+[] spawn de gatinhos
+[x] gato mija e solta bola de pelo
+[x] vasoura limpa mijo e bola de pelo
+
+adicionais
+[] fazer flip do gatineo
+[] spawn de gatinhos CoLoRiDoS
+[] gatineo anda nas diagonais
--- js/main.js
+++ js/main.js
@@ -30,7 +30,7 @@ function create () {
     console.log('create init')
     this.game.physics.startSystem(Phaser.Physics.ARCADE);
     
-    this.life = 3;
+    this.counterLife = 3;
     this.countergatineos = 0;
     this.counterPontos = 0;
 
@@ -41,11 +41,11 @@ function create () {
     this.vasourinha.input.enableDrag();
 
     //Cria itemzinhos
-    this.almofadinha = game.add.sprite(420, 480, 'almofadinha');
-    this.aguinha = game.add.sprite(120, 505, 'aguinha');
-    this.racao = game.add.sprite(020, 440, 'racao');
+    this.almofadinha = game.add.sprite(420, 530, 'almofadinha');
+    this.aguinha = game.add.sprite(140, 555, 'aguinha');
+    this.racao = game.add.sprite(20, 490, 'racao');
     this.caixinha = game.add.sprite(260, 205, 'caixinha');
-    this.brinquedinho = game.add.sprite(620, 430, 'brinquedinho');
+    this.brinquedinho = game.add.sprite(660, 480, 'brinquedinho');
     var bounce = this.brinquedinho.animations.add('bounce');
     this.brinquedinho.animations.play('bounce', 2, true);
 
@@ -55,6 +55,10 @@ function create () {
     //cria gatineo
     this.gatineo = game.add.sprite(game.world.centerX, game.world.centerY, 'gatineo');
     this.countergatineos += 1;
+    this.game.physics.arcade.enable(this.gatineo);
+    this.gatineo.originalPosition =  this.gatineo.position.clone();
+    //this.gatineo.events.onDragStop.add(function(currentSprite))
+
     this.gatineo.inputEnabled = true;
     this.gatineo.input.enableDrag();
     this.gatineo.anchor.setTo(0.5, 0.5);
@@ -62,6 +66,7 @@ function create () {
     walk.enableUpdate = true;
     this.gatineo.animations.play('walk', 3, true);
     this.gatineo.status = -1;
+    this.gatineoTween = game.add.tween(this.gatineo);
 
     //cria balaozinho de status junto ao gatineo
     this.balaozinho = game.add.sprite(0, 0, 'balaozinho');
@@ -70,76 +75,97 @@ function create () {
 
     //text menu
     this.text = game.add.text(100, 120, "Pontos de Atração: " + this.counterPontos
-                                 + '\n Gatineos: ' + this.countergatineos + "  Vidas: " + this.life );
+                                 + '\n Gatineos: ' + this.countergatineos + "  Vidas: " + this.counterLife );
     this.text.fill = '#ffffff';
     this.text.font = 'Arial';
+    this.point = game.add.text(0, 0, "");
+    this.point.alignTo(this.text, Phaser.TOP_RIGHT, 16);
 
     timer = game.time.create(false);
-    timer.loop(3000, changeStatus, this);
-    timer.loop(7000, decreaseLife, this);
-    timer.loop(3000, walking, this);
+    timer.loop(10000, changeStatus, this);
+    timer.loop(6000, walking, this);
+    timer.loop(6000, pee, this);
     timer.start();
 
     console.log('create end')
 }
 
-function walking(){
+function pee(){
+    this.peeOrPelo = Math.floor((Math.random() * 2)) ? game.add.sprite(0, 0, 'bolapelo') : game.add.sprite(0, 0, 'mijo'); 
+    this.peeOrPelo.alignTo(this.gatineo, Phaser.BOTTOM_RIGHT, 16);
+}
 
+function walking(){
 
     var walkDirection = Math.floor((Math.random() * 4) + 1); 
     //Se for 1 ou 2 o gato anda no eito x, se for impar o eixo x e' decrescido
     if (walkDirection <= 2){
-        var x = this.gatineo.x + 100*(Math.pow(-1, walkDirection));
+        //this.gatineo.scale.x *= Math.pow(-1, walkDirection);
+        var positionTweenX = this.gatineo.position.x + 100*(Math.pow(-1, walkDirection));
+        this.gatineoTween.to({ x: positionTweenX}, 3000, 'Linear', true, 0);
     } else {
-        var y = this.gatineo.y + 100*(Math.pow(-1, walkDirection));
+        var positionTweenY = this.gatineo.position.y + 100*(Math.pow(-1, walkDirection));
+        if (positionTweenY > 185 && positionTweenY < 500){
+            this.gatineoTween.to({ y: positionTweenY}, 3000, 'Linear', true, 0);
+        }
     }
-    this.gatineo.rotation = this.game.physics.arcade.movetoXY(this.gatineo, x, y, 300);
-    //this.gatineo.animations.play('walk', 3, true);
     this.gatineo.animations.frame = 1;
-
 }
 
 function changeStatus(){
     if (this.balaozinho.visible == false){
         this.balaozinho.visible = true;
-        status = Math.floor((Math.random() * 5) + 1); 
-        this.gatineo.status = status - 1;
-        this.balaozinho.animations.frame = status - 1;
+        status = Math.floor((Math.random() * 5)); 
+        this.gatineo.status = status;
+        this.balaozinho.animations.frame = status;
+        //timer.loop(5000, decreaseLife(status), this);
     }
     
 }
 
-function decreaseLife(){
-    //TODO: timer de status
+function decreaseLife(status){
+    if (status != -1){
+        this.counterLife -= 1;
+    }
 }
 
 function update () {
 
-
-    if (checkOverlap(this.gatineo, this.brinquedinho)){
-      this.text.setText("Pontos de Atração: " + this.counterPontos
-                                 + '\n Gatineosss: ' + this.countergatineos);
-    } 
-
     this.balaozinho.alignTo(this.gatineo, Phaser.TOP_RIGHT, 16);
 
-    var pontuou = 0;
     this.items.forEach((item, index) => {
         if (checkOverlap(item, this.gatineo)){
-            if ((this.gatineo.status == index) && (pontuou == 0)){
-                this.point = game.add.text(0, 0, "+20");
-                this.point.alignTo(this.gatineo, Phaser.TOP_RIGHT, 16);
+            if ((this.gatineo.status == index)){
+                console.log("memes");
+                this.point.setText("+20");
                 this.counterPontos += 20;
-                pontuou = 1;
+                this.balaozinho.visible = false;
+                this.gatineo.status = -1;
             }
         }
     });
-    //destruir ponto
 
-    //this.gatineo.body.velocity.x = 0;
-    //this.gatineo.body.velocity.y = 0;
+
+    if (this.peeOrPelo){
+        if (checkOverlap(this.peeOrPelo, this.vasourinha) && this.peeOrPelo.visible == true){
+            this.counterPontos += 10;
+            this.peeOrPelo.visible = false;
+            console.log("peeorpelo")
+        }
+    }
+    
+
+    //if (this.point){
+    //    this.point.destroy();
+    //    }
+    //destruir fade?
+    this.text.setText("Pontos de Atração: " + this.counterPontos
+                         + '\n Gatineos: ' + this.countergatineos + '  Vidas: ' + this.counterLife);
+
+    if (this.counterLife == 0){
+        gameOver();
+    }
 }
-
 
 
 function checkOverlap(spriteA, spriteB) {
@@ -148,3 +174,8 @@ function checkOverlap(spriteA, spriteB) {
 
     return Phaser.Rectangle.intersects(boundsA, boundsB);
 }
+
+function gameOver(){
+    this.text.setText("Game Over! " + this.counterPontos + "Pontos de Atração e \n"
+        + this.countergatineos + " Gatineos!");
+};
